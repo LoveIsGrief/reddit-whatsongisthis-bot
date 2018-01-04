a94496315199d223a87a74afd7ca90bc2275a441
LoveIsGrief <LoveIsGrief@users.noreply.github.com>
2018-01-04 20:31:02 +0100

    green test: no start but end
    #3 - Accept clip params for the media download in the post title

--- src/tests/title_parser.py
+++ src/tests/title_parser.py
@@ -22,6 +22,13 @@ class TestRangeParser(unittest.TestCase):
         self.assertEqual(get_range_from_string(string_no_hours), (60, None))
         self.assertEqual(get_range_from_string(string_with_hours), (3661, None))
 
+    def test_no_start_but_end(self):
+        string_no_hours = "prefix [-02:00]"
+        string_with_hours = "prefix [-01:02:02] suffix"
+
+        self.assertEqual(get_range_from_string(string_no_hours), (None, 120))
+        self.assertEqual(get_range_from_string(string_with_hours), (None, 3722))
+
 
 if __name__ == '__main__':
     unittest.main()
